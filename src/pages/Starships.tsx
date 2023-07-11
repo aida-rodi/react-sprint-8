f5c356b9116fa0e1f3b56a6a24f223a63ec29322
aida-rodi <aida.rodi.1997@gmail.com>
2023-07-11 11:36:43 +0200

    exercise 3

--- src/pages/Starships.tsx
+++ src/pages/Starships.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Navbar } from '../components/Navbar';
 import axios from 'axios';
-import "reactjs-popup/dist/index.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { StarshipInfoCard } from '../components/StarshipInfoCard';
 
 interface Starship {
@@ -11,16 +11,39 @@ interface Starship {
 }
 
 function Starships() {
-  const [starships, setStarships] = useState<Starship[] | null>(null);
+  const [starships, setStarships] = useState<Starship[]>([]);
   const [selectedStarship, setSelectedStarship] = useState<Starship>();
+  const [page, setPage] = useState<number>(1);
+  const [hasMore, setHasMore] = useState<boolean>(false);
 
-  function getStarships() {
-    axios.get('https://swapi.dev/api/starships/')
-    .then(res => {
-      console.log(res.data.results);
-      setStarships(res.data.results as Starship[]);
-    });
+  const callAPI = async(page: number) => {
+    const res = await axios.get(`https://swapi.dev/api/starships/?page=${page}`)
+    console.log(res.data);
+    return res.data
   }
+
+  const getStarships = async(page: number) => {
+    const data = await callAPI(page)
+    setStarships([...starships as Starship[], ...data.results] as Starship[])
+    setHasMore(true)
+  };
+
+  const getMoreShips = async() => {
+		const newPage = page + 1;
+    const additionalStarships = await callAPI(newPage);
+    const results = additionalStarships.results;
+    console.log('page:', newPage);
+    console.log('results.length:', results.length);
+
+		if (additionalStarships.next === null) {
+			setHasMore(false);
+      setStarships((previousStarships) => [...previousStarships, ...results]);
+		} else {
+			setPage(newPage);
+			setStarships((previousStarships) => [...previousStarships, ...results]);
+		}
+    console.log('hasmore', hasMore);
+	};
 
   function handleStarshipClick(starship: Starship) {
     if (selectedStarship && selectedStarship.name === starship.name) {
@@ -31,26 +54,33 @@ function Starships() {
   };
 
   useEffect( () => {
-    getStarships();
+    getStarships(page);
   }, [])
   
   return (
     <>
       <Navbar />
-      <div className='starshipsPageDiv'>
-        { starships === null && <div className='loading'>Loading...</div> }
-        {starships?.map((starship, index) => {
-          return (
-            <div key={index} className='starshipInfoDiv'onClick={() => handleStarshipClick(starship)}>
-              <div className='starshipName'>
-                <a>{starship.name.toUpperCase()}</a>
-              </div>
-              <div className='starshipModel'>{starship.model}</div>
-              {selectedStarship && selectedStarship.name === starship.name && <StarshipInfoCard selectedStarship={selectedStarship} />}
-            </div>
-          );
-        })}
-      </div>
+      <InfiniteScroll
+        dataLength={starships.length}
+        hasMore={hasMore}
+        next={getMoreShips}
+        loader={<div className='loadingMore'>Loading more starships...</div>}>
+
+          <div className='starshipsPageDiv'>
+            { starships.length === 0 && <div className='loading'>Loading...</div> }
+            {starships?.map((starship, index) => {
+              return (
+                <div key={index} className='starshipInfoDiv'onClick={() => handleStarshipClick(starship)}>
+                  <div className='starshipName'>
+                    <a>{starship.name.toUpperCase()}</a>
+                  </div>
+                  <div className='starshipModel'>{starship.model}</div>
+                  {selectedStarship && selectedStarship.name === starship.name && <StarshipInfoCard selectedStarship={selectedStarship} />}
+                </div>
+              );
+            })}
+          </div>
+      </InfiniteScroll>
     </>
   );
 }
